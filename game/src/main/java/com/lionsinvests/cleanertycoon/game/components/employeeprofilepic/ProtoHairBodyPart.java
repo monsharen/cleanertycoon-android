d872fb6a23ce96861e80f291f22dd6e37b098cd8
monsharen <thomas.rosenquist@flexionmobile.com>
2018-08-24 12:24:55 +0100

    Better hair

--- game/src/main/java/com/lionsinvests/cleanertycoon/game/components/employeeprofilepic/ProtoHairBodyPart.java
+++ game/src/main/java/com/lionsinvests/cleanertycoon/game/components/employeeprofilepic/ProtoHairBodyPart.java
@@ -20,5 +20,18 @@ public class ProtoHairBodyPart extends BaseBodyPart {
         pixelCanvas.drawLine(4, 6, 8);
         pixelCanvas.drawLine(4, 7, 2);
         pixelCanvas.drawBox(3, 8, 2, 5);
+        pixelCanvas.drawPixel(7, 7);
+        pixelCanvas.drawPixel(9, 7);
+        pixelCanvas.drawPixel(11, 7);
+        pixelCanvas.drawPixel(12, 8);
+
+        pixelCanvas.drawPixel(4, 13);
+        pixelCanvas.drawPixel(4, 14);
+        pixelCanvas.drawPixel(3, 15);
+        pixelCanvas.drawPixel(2, 15);
+        pixelCanvas.drawPixel(1, 14);
+
+        pixelCanvas.drawPixel(5, 11);
+        pixelCanvas.drawPixel(6, 12);
     }
 }
